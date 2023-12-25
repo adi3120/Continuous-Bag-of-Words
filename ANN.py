5cef9732d6790e0017bc97829cd90c42e02ac814
adi3120 <aditya.yadav.cs.20@ggits.net>
2023-12-25 11:37:38 +0530

    added files

---
+++ ANN.py
@@ -0,0 +1,503 @@
+# import here
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+def SigmoidActivation(x):
+    return 1 / (1 + np.exp(-np.clip(x, -500, 500)))
+
+
+def ReLUActivation(x):
+    return np.maximum(0, x)
+
+
+def noActivation(x):
+    return x
+
+
+def TanhActivation(x):
+    return np.tanh(x)
+
+
+def SoftmaxActivation(x):
+    exp_values = np.exp(x - np.max(x))
+    activations = exp_values / np.sum(exp_values)
+    return activations
+
+
+def sigmoid_derivative(x):
+    sigmoid_x = SigmoidActivation(x)
+    return sigmoid_x * (1 - sigmoid_x)
+
+
+def ReLUDerivative(x):
+    return np.where(x <= 0, 0, 1)
+
+
+def noActDerivative(x):
+    return np.ones((1, len(x)))
+
+
+def TanhDerivative(x):
+    return 1 - np.tanh(x) ** 2
+
+
+def softmax_derivative(x):
+    n = np.size(x)
+    exp_values = np.exp(x - np.max(x))
+    softmax_output = exp_values / np.sum(exp_values)
+    derivative = softmax_output * (np.identity(n) - softmax_output.T)
+    return derivative
+
+
+# def softmax_derivative(x):
+#   n=np.size(x)
+#   tmp=np.tile(x,n).reshape(n,n)
+#   derivative=tmp*(np.identity(n)-np.transpose(tmp))
+#   return derivative
+
+
+def MSE_Derivative(activations, actual):
+    y = actual
+    activations = activations[0]
+    dLdy = 0
+    for i in range(0, len(y)):
+        dLdy += (2 / len(activations)) * (activations[i] - y[i])
+    return dLdy
+
+
+def Binary_CrossEntropy_Derivative(activations, actual):
+    activations = activations[0]
+    epsilon = 1e-10
+    activations = np.clip(
+        activations, epsilon, 1 - epsilon
+    )  # Avoiding division by zero
+    return np.array(
+        (activations - actual) / (activations * (1 - activations) + epsilon)
+    ).reshape(-1, 1)
+
+
+def CrossEntropy_Derivative(activations, actual):
+    epsilon = 1e-10
+    activations = activations[0]
+    activations = np.clip(
+        activations, epsilon, 1 - epsilon
+    )  # Avoiding division by zero
+    derivative = np.zeros_like(activations)
+    tc = -1
+    for i in range(0, len(actual)):
+        if actual[i] == 1:
+            tc = i
+            break
+    derivative[tc] = -actual[tc] / activations[tc]
+    derivative = derivative.reshape(-1, 1)
+    return derivative
+
+
+def MSE_Loss(activations, actual):
+    loss = 0
+    activations = activations[0]
+    for i in range(len(activations)):
+        loss += (activations[i] - actual[i]) ** 2
+    return (1 / len(activations)) * loss
+
+
+def Binary_CrossEntropy_Loss(activations, actual):
+    epsilon = 1e-10
+    activations = activations[0]
+    activations = np.clip(
+        activations, epsilon, 1 - epsilon
+    )  # Avoiding logarithm of zero
+    loss = -np.sum(
+        actual * np.log(activations) + (1 - actual) * np.log(1 - activations)
+    )
+    return loss / len(activations)
+
+
+def CrossEntropy_Loss(activations, actual):
+    activations = activations[0]
+    tc = -1
+    for i in range(0, len(actual)):
+        if actual[i] == 1:
+            tc = i
+            break
+    epsilon = 1e-10  # small value to prevent log(0)
+    activations = np.clip(
+        activations, epsilon, 1 - epsilon
+    )  # clip to avoid log(0) or log(1)
+    ce_loss = -(actual[tc] * np.log(activations[tc]))
+    return ce_loss
+
+
+class Layer:
+    def __init__(self, n):
+        self.length = n
+
+
+class InputLayer:
+    def __init__(self, n, actfn="none"):
+        self.length = n
+        self.inputs = np.array([])
+        self.next = None
+        self.actname = ""
+        if actfn == "sigmoid":
+            self.actfn = SigmoidActivation
+            self.actname = "sigmoid"
+        elif actfn == "relu":
+            self.actfn = ReLUActivation
+            self.actname = "relu"
+        elif actfn == "tanh":
+            self.actfn = TanhActivation
+            self.actname = "tanh"
+        elif actfn == "none":
+            self.actfn = noActivation
+            self.actname = "none"
+
+    def put_values(self, values):
+        if len(values) == self.length:
+            self.inputs = np.array([values])
+        else:
+            print(
+                "Error: The values you are trying to insert are more then the allocated size of input vector"
+            )
+
+    def forward(self):
+        self.pre_activations = self.inputs
+        self.pre_activations = np.squeeze(self.pre_activations)
+        self.activations = self.actfn(self.pre_activations)
+
+
+class HiddenLayer:
+    def __init__(self, n, actfn="none"):
+        self.length = n
+        self.Bias = None
+        self.next = None
+        self.actname = ""
+        if actfn == "sigmoid":
+            self.actfn = SigmoidActivation
+            self.actname = "sigmoid"
+        elif actfn == "relu":
+            self.actfn = ReLUActivation
+            self.actname = "relu"
+        elif actfn == "tanh":
+            self.actfn = TanhActivation
+            self.actname = "tanh"
+        elif actfn == "none":
+            self.actfn = noActivation
+            self.actname = "none"
+
+    def attach_after(self, layer):
+        self.previous = layer
+        self.previous.next = self
+
+    def set_weights(self, method="random"):
+        if self.length > 0:
+            if method == "normal_random":
+                self.W = np.random.randn(self.length, self.previous.length)
+            elif method == "uniform_random":
+                self.W = np.random.rand(self.length, self.previous.length)
+            elif method == "xavier":
+                self.W = (1 / self.length**0.5) * np.random.randn(
+                    self.length, self.previous.length
+                )
+            elif method == "one":
+                self.W = np.ones((self.length, self.previous.length))
+            elif method == "he":
+                self.W = np.random.randn(self.length, self.previous.length) * np.sqrt(
+                    2.0 / (self.previous.length * self.length)
+                )
+            elif method == "lecun":
+                limit = np.sqrt(1.0 / self.previous.length)
+                self.W = np.random.uniform(
+                    -limit, limit, (self.length, self.previous.length)
+                )
+
+    def set_biases(self, method="random"):
+        if self.length > 0 and self.previous != None:
+            if method == "normal_random":
+                self.Bias = np.random.randn(1, self.length)
+            elif method == "uniform_random":
+                self.Bias = np.random.rand(1, self.length)
+            elif method == "zeros":
+                self.Bias = np.zeros((1, self.length))
+            elif method == "constant":
+                self.Bias = np.full(
+                    (1, self.length), 0.1
+                )  # Set bias to a constant value
+            elif method == "xavier":
+                self.Bias = np.random.randn(1, self.length) * np.sqrt(1 / self.length)
+            elif method == "lecun":
+                self.Bias = np.random.randn(1, self.length) * np.sqrt(1 / self.length)
+            elif method == "he":
+                self.Bias = np.random.randn(1, self.length) * np.sqrt(1 / self.length)
+            # Add other bias initialization methods here...
+
+    def forward(self):
+        self.pre_activations = np.dot(self.W, self.previous.activations) + self.Bias
+        self.pre_activations = np.squeeze(self.pre_activations)
+        self.activations = self.actfn(self.pre_activations)
+
+    def backward(self):
+        nextdLda = self.next.dLda
+        dadh = self.next.W
+        if self.actname == "relu":
+            dhda = ReLUDerivative(self.pre_activations)
+        elif self.actname == "sigmoid":
+            dhda = sigmoid_derivative(self.pre_activations)
+        elif self.actname == "tanh":
+            dhda = TanhDerivative(self.pre_activations)
+        elif self.actname == "none":
+            dhda = noActDerivative(self.pre_activations)
+        dadW = self.previous.activations
+
+        dhda = dhda.reshape(-1, 1)
+        dadW = dadW.reshape(-1, 1)
+        dLdh = np.dot(dadh.T, nextdLda)
+        dLda = np.multiply(dLdh, dhda)
+        dLdW = np.dot(dLda, dadW.T)
+        self.dLda = dLda
+        self.dLdW = dLdW
+
+
+class OutputLayer:
+    def __init__(self, n, outputfn="none", lossfn="MSE"):
+        self.length = n
+        self.previous = None
+        self.W = None
+        self.next = None
+        self.Bias = None
+        self.Loss_grad_W = None
+        self.lossfnname = ""
+
+        self.actname = ""
+        if outputfn == "sigmoid":
+            self.outputfn = SigmoidActivation
+            self.actname = "sigmoid"
+        elif outputfn == "relu":
+            self.outputfn = ReLUActivation
+            self.actname = "relu"
+        elif outputfn == "none":
+            self.outputfn = noActivation
+            self.actname = "none"
+        elif outputfn == "softmax":
+            self.outputfn = SoftmaxActivation
+            self.actname = "softmax"
+        elif outputfn == "tanh":
+            self.outputfn = TanhActivation
+            self.actname = "tanh"
+
+        if lossfn == "MSE":
+            self.lossfn = MSE_Loss
+            self.lossfnname = "MSE"
+        elif lossfn == "bincrossentropy":
+            self.lossfn = Binary_CrossEntropy_Loss
+            self.lossfnname = "bincrossentropy"
+        elif lossfn == "crossentropy":
+            self.lossfn = CrossEntropy_Loss
+            self.lossfnname = "crossentropy"
+
+    def attach_after(self, layer):
+        self.previous = layer
+        self.previous.next = self
+
+    def set_weights(self, method="random"):
+        if self.length > 0 and self.previous != None:
+            if method == "normal_random":
+                self.W = np.random.randn(self.length, self.previous.length)
+            elif method == "uniform_random":
+                self.W = np.random.rand(self.length, self.previous.length)
+            elif method == "xavier":
+                self.W = (1 / self.length**0.5) * np.random.randn(
+                    self.length, self.previous.length
+                )
+            elif method == "one":
+                self.W = np.ones((self.length, self.previous.length))
+            elif method == "he":
+                self.W = np.random.randn(self.length, self.previous.length) * np.sqrt(
+                    2.0 / self.previous.length
+                )
+            elif method == "lecun":
+                limit = np.sqrt(1.0 / self.previous.length)
+                self.W = np.random.uniform(
+                    -limit, limit, (self.length, self.previous.length)
+                )
+
+    def set_biases(self, method="random"):
+        if self.length > 0 and self.previous != None:
+            if method == "normal_random":
+                self.Bias = np.random.randn(1, self.length)
+            elif method == "uniform_random":
+                self.Bias = np.random.rand(1, self.length)
+            elif method == "zeros":
+                self.Bias = np.zeros((1, self.length))
+            elif method == "constant":
+                self.Bias = np.full(
+                    (1, self.length), 0.1
+                )  # Set bias to a constant value
+            elif method == "xavier":
+                self.Bias = np.random.randn(1, self.length) * np.sqrt(1 / self.length)
+            elif method == "lecun":
+                self.Bias = np.random.randn(1, self.length) * np.sqrt(
+                    1 / self.previous.length
+                )
+            # Add other bias initialization methods here...
+
+    def forward(self):
+        self.pre_activations = np.dot(self.W, self.previous.activations) + self.Bias
+        self.activations = self.outputfn(self.pre_activations)
+
+    def output(self):
+        return self.activations
+
+    def set_actual(self, actual):
+        self.actual = actual
+
+    def loss(self):
+        return self.lossfn(self.activations, self.actual)
+
+    def backward(self):
+        if self.lossfnname == "MSE":
+            dLdy = MSE_Derivative(self.activations, self.actual)
+        elif self.lossfnname == "bincrossentropy":
+            dLdy = Binary_CrossEntropy_Derivative(self.activations, self.actual)
+        elif self.lossfnname == "crossentropy":
+            dLdy = CrossEntropy_Derivative(self.activations, self.actual)
+
+        if self.actname == "sigmoid":
+            dyda = sigmoid_derivative(self.pre_activations[0])
+        elif self.actname == "relu":
+            dyda = ReLUDerivative(self.pre_activations[0])
+        elif self.actname == "none":
+            dyda = noActDerivative(self.pre_activations[0])
+        elif self.actname == "softmax":
+            dyda = softmax_derivative(self.pre_activations[0])
+        elif self.actname == "tanh":
+            dyda = TanhDerivative(self.pre_activations)
+
+        dadW = self.previous.activations
+
+        dadW = dadW.reshape(-1, 1)
+
+        if self.actname != "softmax":
+            dyda = dyda.reshape(-1, 1)
+            dLda = dLdy * dyda
+        else:
+            dyda = dyda
+            dLda = self.activations - self.actual
+            dLda = dLda.reshape(-1, 1)
+
+        dLdW = np.dot(dLda, dadW.T)
+
+        self.dLdy = dLdy
+        self.dyda = dyda
+        self.dLda = dLda
+        self.dLdW = dLdW
+
+
+def gradient_descent_threshold(ANN, x, y, eta, thresh):
+    loss = []
+    ANN[0].put_values(x[0])
+    ANN[len(ANN) - 1].set_actual(y[0])
+
+    for layer in ANN:
+        layer.forward()
+    j = 0
+    while ANN[-1].loss() > thresh:
+        if len(loss) > 2 and loss[-1] > loss[-2]:
+            break
+        for k in range(0, len(x)):
+            ANN[0].put_values(x[k])
+            ANN[len(ANN) - 1].set_actual(y[k])
+
+            for layer in ANN:
+                layer.forward()
+
+            for i in range(len(ANN) - 1, 0, -1):
+                ANN[i].backward()
+
+            for i in range(1, len(ANN)):
+                ANN[i].W -= eta * ANN[i].dLdW
+                ANN[i].Bias -= eta * ANN[i].dLda.reshape(1, -1)
+
+        loss.append(ANN[len(ANN) - 1].loss())
+        j += 1
+        print(f"epoch: {j}, Loss: {ANN[len(ANN)-1].loss()}")
+    return ANN, loss
+
+
+def gradient_descent_epoch(ANN, x, y, eta, epochs):
+    loss = []
+    for j in range(0, epochs):
+        corrects = 0
+        for k in range(0, len(x)):
+            ANN[0].put_values(x[k])
+            ANN[len(ANN) - 1].set_actual(y[k])
+
+            for layer in ANN:
+                layer.forward()
+            #   print("Predicted: ",ANN[-1].output())
+            #   print("Actual: ",y[k])
+
+            actual_label = np.argmax(y[k])
+            output = ANN[-1].output()
+            predicted_label = np.argmax(output)
+            #   print("Actual: ",actual_label)
+            #   print("output: ",output)
+            #   print("predicted_label: ",predicted_label)
+
+            if predicted_label == actual_label:
+                corrects += 1
+
+            #   print("Input no: ",k)
+            #   print("Activations: ",ANN[-1].activations)
+            #   print("Output: ",ANN[-1].output())
+
+            for i in range(len(ANN) - 1, 0, -1):
+                ANN[i].backward()
+
+            for i in range(1, len(ANN)):
+                ANN[i].W -= eta * ANN[i].dLdW
+                ANN[i].Bias -= eta * ANN[i].dLda.reshape(1, -1)
+
+        loss.append(ANN[len(ANN) - 1].loss())
+        print(
+            f"epoch: {j}, Loss: {ANN[len(ANN)-1].loss()}, Accuracy: {100*corrects/len(y)}"
+        )
+    return ANN, loss
+
+
+def gradient_descent_epoch_minibatch(ANN, x, y, eta, epochs, batch_size):
+    loss = []
+    batch_completed = 0
+    for j in range(0, epochs):
+        weight_gradient=[]
+        bias_gradient=[]
+        corrects = 0
+        for k in range(0, len(x)):
+            ANN[0].put_values(x[k])
+            ANN[len(ANN) - 1].set_actual(y[k])
+
+            for layer in ANN:
+                layer.forward()
+
+            actual_label = np.argmax(y[k])
+            output = ANN[-1].output()
+            predicted_label = np.argmax(output)
+
+            if predicted_label == actual_label:
+                corrects += 1
+
+            for i in range(len(ANN) - 1, 0, -1):
+                ANN[i].backward()
+            # for i in range(1,len(ANN)):
+
+            for i in range(1, len(ANN)):
+                ANN[i].W -= eta * ANN[i].dLdW
+                ANN[i].Bias -= eta * ANN[i].dLda.reshape(1, -1)
+
+        loss.append(ANN[len(ANN) - 1].loss())
+        print(
+            f"epoch: {j}, Loss: {ANN[len(ANN)-1].loss()}, Accuracy: {100*corrects/len(y)}"
+        )
+    return ANN, loss
---
+++ CBOW.py
@@ -0,0 +1,277 @@
+# import here
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+nltk.download('wordnet')
+import pandas as pd
+import math
+from numpy.linalg import eig
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import plotly.express as px
+from ANN import *
+
+
+
+stop_words=["i","me","my","myself","we","our","ours","ourselves","you","your","yours","yourself","yourselves","he","him","his","himself","she","her","hers","herself","it","its","itself","they","them","their","theirs","themselves","what","which","who","whom","this","that","these","those","am","is","are","was","were","be","been","being","have","has","had","having","do","does","did","doing","a","an","the","and","but","if","or","because","as","until","while","of","at","by","for","with","about","against","between","into","through","during","before","after","above","below","to","from","up","down","in","out","on","off","over","under","again","further","then","once","here","there","when","where","why","how","all","any","both","each","few","more","most","other","some","such","no","nor","not","only","own","same","so","than","too","very","s","t","can","will","just","don","should","now"]
+
+def corpus_remove_punc_symb(corpus):
+  punctuations='''!()-[]{};:'"\,<>./?@#$%^&*~'''
+  no_punct=""
+  for char in corpus:
+    if char not in punctuations:
+      no_punct+=char
+  return no_punct
+
+def corpus_tokenise(corpus):
+  tokens=corpus.split()
+  return tokens
+
+def corpus_remove_stop_words(corpus):
+  coList=corpus_tokenise(corpus)
+  for word in coList:
+    if word in stop_words:
+      coList.remove(word)
+  return " ".join(coList)
+
+def corpus_lemmatize(corpus):
+  lemmatizer = WordNetLemmatizer()
+  tokens=corpus_tokenise(corpus)
+  for i in range(0,len(tokens)):
+    tokens[i]=lemmatizer.lemmatize(tokens[i])
+  return " ".join(tokens)
+
+
+def corpus_prepare(corpus):
+  corpus=corpus.lower()
+  corpus=corpus_remove_punc_symb(corpus)
+  corpus=corpus_remove_stop_words(corpus)
+  corpus=corpus_lemmatize(corpus)
+  return corpus
+
+def vocabulary_prepare(corpus):
+  vocabulary=list(set(corpus_tokenise(corpus)))
+  return vocabulary
+
+def countOccurrences(str, word):
+    wordslist = list(str.split())
+    return wordslist.count(word)
+
+def co_matrix_prepare(corpus,vocabulary,window_size):
+  matrix={}
+  context={}
+  for i in range(0,len(vocabulary)):
+    context[vocabulary[i]]=0
+
+  for i in range(0,len(vocabulary)):
+    matrix[vocabulary[i]]=context
+
+  matrix = pd.DataFrame.from_dict(matrix)
+  corpus_list=corpus_tokenise(corpus)
+
+  for i in range(0,len(corpus_list)):
+    windowwords=[]
+    if i+window_size<=len(corpus_list):
+      for j in range(0,window_size):
+        windowwords.append(corpus_list[i+j])
+        matrix[corpus_list[i]][corpus_list[i+j]]+=1
+    else:
+      break
+
+  for i in matrix.keys():
+    matrix[i][i]=0
+
+  return matrix
+
+def prepare_one_hot_inputs(vocabulary):
+  inputs={}
+  for i,j in enumerate(vocabulary):
+    inputs[j]=np.zeros(len(vocabulary))
+    inputs[j][i]=1
+  return inputs
+
+def prepare_outputs(matrix):
+  outputs={}
+  for word in matrix.keys():
+      outputs[word]=matrix[word]
+  return outputs
+
+def co_matrix_probabilities(matrix, corpus):
+    for word in matrix.keys():
+        total_word_occurrences = countOccurrences(corpus, word)
+        for context in matrix[word].keys():
+            matrix[word][context] = matrix[word][context] / total_word_occurrences
+    return matrix
+
+corpus = """Dogs are wonderful companions .
+They enjoy playing fetch and running in the park.
+ A well-trained dog can learn many tricks.
+ Cats are independent creatures, often preferring solitude.
+ Their agility and grace are admired by many.
+ Some people love both dogs and cats equally, while others have a strong preference for one over the other.
+ Dogs require regular exercise, whereas cats are more low-maintenance.
+ Dog owners often take their pets for walks, while cats enjoy lounging indoors.
+ The debate between dog lovers and cat enthusiasts is never-ending, each having valid reasons for their preferences.
+ Nevertheless, both dogs and cats bring joy and comfort to countless households.
+"""
+
+corpus=corpus_prepare(corpus)
+vocabulary=vocabulary_prepare(corpus)
+
+
+co_matrix=co_matrix_prepare(corpus,vocabulary,2)
+co_matrix=co_matrix_probabilities(co_matrix,corpus)
+
+inputs=prepare_one_hot_inputs(vocabulary)
+outputs=prepare_outputs(co_matrix)
+
+V=len(vocabulary)
+
+k=30
+
+eta=0.001
+
+i=InputLayer(V,"none")
+
+h1=HiddenLayer(k,"none")
+h1.attach_after(i)
+h1.set_weights("normal_random")
+h1.set_biases("zeros")
+
+o=OutputLayer(V,"softmax","crossentropy")
+o.attach_after(h1)
+o.set_weights("normal_random")
+o.set_biases("zeros")
+
+ANN=[i,h1,o]
+
+def gradient_descent_CBOW_epoch(ANN, inputs, outputs, eta, epochs):
+    loss = []
+    for j in range(0, epochs):
+        corrects = 0
+        for key in inputs.keys():
+            ANN[0].put_values(inputs[key])
+            ANN[len(ANN) - 1].set_actual(outputs[key].values)
+
+            for layer in ANN:
+                layer.forward()
+
+            actual_label = np.argmax(outputs[key].values)
+            output = ANN[-1].output()
+            predicted_label = np.argmax(output)
+
+            if predicted_label == actual_label:
+                corrects += 1
+
+            for i in range(len(ANN) - 1, 0, -1):
+                ANN[i].backward()
+
+            for i in range(1, len(ANN)):
+                ANN[i].W -= eta * ANN[i].dLdW
+                # ANN[i].Bias -= eta * ANN[i].dLda.reshape(1, -1)
+
+        loss.append(ANN[len(ANN) - 1].loss())
+        print(
+            f"epoch: {j}, Loss: {ANN[len(ANN)-1].loss()}, Accuracy: {100*corrects/len(outputs)}"
+        )
+    return ANN, loss
+
+
+ANN,loss=gradient_descent_CBOW_epoch(ANN,inputs,outputs,eta,5000)
+
+plt.plot(loss)
+plt.xlabel("epochs")
+plt.ylabel("loss")
+plt.show()
+
+testinput='dog'
+ANN[0].put_values(inputs[testinput])
+for i in ANN:
+  i.forward()
+localoutput={}
+ANNoutput=ANN[-1].output()
+for i in range(0,len(vocabulary)):
+  localoutput[vocabulary[i]]=ANNoutput[0][i]
+sorted_output = dict(sorted(localoutput.items(), key=lambda item: item[1],reverse=True))
+
+labels = list(sorted_output.keys())
+probabilities = list(sorted_output.values())
+
+# Plotting the probabilities for each label
+plt.figure(figsize=(10, 20))
+plt.barh(labels, probabilities, color='skyblue')
+plt.xlabel('Probability')
+plt.ylabel('Labels')
+plt.title(f'Probabilities of Labels |  Input :{testinput} ')
+plt.gca().invert_yaxis()  # Invert y-axis for better readability
+plt.show()
+
+def plot_weight_matrices(ANN):
+    num_layers = len(ANN)
+
+    plt.figure(figsize=(15, 10))
+    for i in range(1, num_layers):
+        plt.subplot(2, (num_layers + 1) // 2, i)
+        plt.imshow(ANN[i].W, cmap='viridis', aspect='auto')
+        plt.title(f"Layer {i} Weights, Shape: {ANN[i].W.shape[0]} x {ANN[i].W.shape[1]}")
+        plt.colorbar()
+
+    plt.tight_layout()
+    plt.show()
+
+plot_weight_matrices(ANN)
+
+
+def get_word_embeddings(vocabulary):
+  word_embeddings={}
+  for i in range(0,len(vocabulary)):
+    word_embeddings[vocabulary[i]]=ANN[-1].W[i]
+  word_embeddings = pd.DataFrame.from_dict(word_embeddings)
+
+  return word_embeddings
+
+word_embeddings=get_word_embeddings(vocabulary)
+
+word_embeddings = word_embeddings.T
+
+# Perform t-SNE on word embeddings to reduce dimensions to 2
+tsne = TSNE(n_components=2,random_state=0)
+word_embeddings_2d = tsne.fit_transform(word_embeddings)
+
+# Plotting the 2D word embeddings
+plt.figure(figsize=(12, 8))
+plt.scatter(word_embeddings_2d[:, 0], word_embeddings_2d[:, 1], alpha=0.5)
+
+# Annotate points with words
+for i, word in enumerate(word_embeddings.index):
+    plt.annotate(word, (word_embeddings_2d[i, 0], word_embeddings_2d[i, 1]))
+
+plt.title('Visualization of Word Embeddings using t-SNE')
+plt.xlabel('Dimension 1')
+plt.ylabel('Dimension 2')
+plt.show()
+
+tsne_3d = TSNE(n_components=3,random_state=0)
+word_embeddings_3d = tsne_3d.fit_transform(word_embeddings)
+
+# Create a DataFrame with the 3D word embeddings
+word_embeddings_3d_df = pd.DataFrame(
+    {
+        'x': word_embeddings_3d[:, 0],
+        'y': word_embeddings_3d[:, 1],
+        'z': word_embeddings_3d[:, 2],
+        'word': word_embeddings.index  # Assuming your index contains words
+    }
+)
+
+# Plotting the 3D word embeddings using Plotly
+fig = px.scatter_3d(word_embeddings_3d_df, x='x', y='y', z='z', text='word')
+fig.update_traces(textposition='top center', marker=dict(size=5))
+fig.update_layout(
+    title='Word Embeddings in 3D',
+    scene=dict(xaxis_title='Dimension 1', yaxis_title='Dimension 2', zaxis_title='Dimension 3')
+)
+fig.show()
+
+print(word_embeddings.T)
+
